0a4b53b69f1580dda3f3dfe63832d1970c5fce43
VSemenov95 <semasvs18@gmail.com>
2021-03-31 21:14:25 +0300

    Создал массив из 5 элементов. Поменял местами первый элемент с последним. Вычислил сумму первого и среднего элементов.

---
+++ src/MyArray.java
@@ -0,0 +1,11 @@
+public class MyArray {
+    public static void main(String[] args) {
+        byte [] MyArray = {5, 7, 3, -4, 9};
+        int length = MyArray.length;
+        byte a = MyArray[0];
+        MyArray[0] = MyArray[length-1];
+        MyArray[length-1] = MyArray[0];
+        int sum = MyArray[0] + MyArray[length-3];
+        System.out.println("Сумма первого и среднего элементов массива = "+ sum);
+    }
+}
