00bb425f6c1cd30b314d2da8657788f80c2a59b3
VSemenov95 <semasvs18@gmail.com>
2021-04-01 19:47:28 +0300

    Исправил ошибку с перестановкой первого и последнего элементов.

--- src/MyArray.java
+++ src/MyArray.java
@@ -4,7 +4,7 @@ public class MyArray {
         int length = MyArray.length;
         byte a = MyArray[0];
         MyArray[0] = MyArray[length-1];
-        MyArray[length-1] = MyArray[0];
+        MyArray[length-1] = a;
         int sum = MyArray[0] + MyArray[length-3];
         System.out.println("Сумма первого и среднего элементов массива = "+ sum);
     }
